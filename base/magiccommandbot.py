5dc308ea7b61770d66585b97c32a7f551cd33e7a
Justin Buist <justin.buist@gmail.com>
2025-05-11 19:14:58 -0400

    Leaning this up a bit

--- base/magiccommandbot.py
+++ base/magiccommandbot.py
@@ -4,26 +4,10 @@ from magicbot import MagicRobot
 class MagicCommandRobot(MagicRobot):
     import commands2
 
-    def robotInit(self) -> None:
-        super().robotInit()
-
-    def createSubsystems(self) -> None:
-        raise NotImplementedError(
-            "You must implement createSubsystems() in your robot class"
-        )
 
     def robotPeriodic(self):
         commands2.CommandScheduler.getInstance().run()
         return super().robotPeriodic()
-
-    def registerSubsystem(self, subsystem: commands2.SubsystemBase) -> None:
-        """
-        Register a subsystem with the command scheduler.
-
-        :param subsystem: The subsystem to register
-        """
-        inst = commands2.CommandScheduler.getInstance()
-        inst.registerSubsystem(subsystem)
 
     def scheduleCommand(self, command: commands2.Command) -> None:
         """
